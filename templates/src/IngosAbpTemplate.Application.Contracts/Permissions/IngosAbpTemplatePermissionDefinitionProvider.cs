bbddeeb9463964666ab19df136341db7192bffe9
Danvic Wang <danvic96@hotmail.com>
2021-03-21 16:26:18 +0800

    Add permision group define code

---
+++ templates/src/IngosAbpTemplate.Application.Contracts/Permissions/IngosAbpTemplatePermissionDefinitionProvider.cs
@@ -0,0 +1,22 @@
+﻿using IngosAbpTemplate.Domain.Shared.Localization;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Localization;
+
+namespace IngosAbpTemplate.Application.Contracts.Permissions
+{
+    public class IngosAbpTemplatePermissionDefinitionProvider : PermissionDefinitionProvider
+    {
+        public override void Define(IPermissionDefinitionContext context)
+        {
+            var myGroup = context.AddGroup(IngosAbpTemplatePermissions.GroupName);
+
+            //Define your own permissions here. Example:
+            //myGroup.AddPermission(IngosAbpTemplatePermissions.MyPermission1, L("Permission:MyPermission1"));
+        }
+
+        private static LocalizableString L(string name)
+        {
+            return LocalizableString.Create<IngosAbpTemplateResource>(name);
+        }
+    }
+}
---
+++ templates/src/IngosAbpTemplate.Application.Contracts/Permissions/IngosAbpTemplatePermissions.cs
@@ -0,0 +1,10 @@
+﻿namespace IngosAbpTemplate.Application.Contracts.Permissions
+{
+    public static class IngosAbpTemplatePermissions
+    {
+        public const string GroupName = "IngosAbpTemplate";
+
+        //Add your own permission names. Example:
+        //public const string MyPermission1 = GroupName + ".MyPermission1";
+    }
+}
